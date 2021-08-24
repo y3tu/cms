ce41c6f1c05134b6f192b708e8a7972a5410a4d2
y3tu <14643092@qq.com>
2021-08-24 16:44:58 +0800

    Initial commit

---
+++ src/main/java/com/y3tu/cms/CmsApplication.java
@@ -0,0 +1,18 @@
+package com.y3tu.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author y3tu
+ */
+@SpringBootApplication
+public class CmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class, args);
+    }
+
+}
