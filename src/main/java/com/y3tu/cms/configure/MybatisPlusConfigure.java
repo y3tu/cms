08259ee4bd261610fb844ede9e4ef624e5b96e73
y3tu <14643092@qq.com>
2021-09-06 16:33:01 +0800

    update

---
+++ src/main/java/com/y3tu/cms/configure/MybatisPlusConfigure.java
@@ -0,0 +1,29 @@
+package com.y3tu.cms.configure;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置
+ *
+ * @author y3tu
+ */
+@Configuration
+@MapperScan("com.y3tu.cms.*.mapper")
+public class MybatisPlusConfigure {
+
+    /**
+     * mybatis-plus 分页插件配置
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
